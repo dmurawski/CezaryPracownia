716fad845e162818d5c83baede3714a693a0fa89
dmurawski <dmurawski018@gmail.com>
2022-04-21 15:15:43 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React , { useEffect, useState } from 'react';
 import './App.css';
+import { getList ,setItem } from './services/list';
+
 
 function App() {
+  const [list, setList] = useState([]);
+  const [itemInput, setItemInput] = useState('');
+  const [alert, setAlert] = useState(false);
+  
+  useEffect(() => {
+    if (alert) {
+      setTimeout(() => {
+        setAlert(false);
+      }, 1000)
+    }
+  }, [alert])
+
+
+
+  useEffect(() => {
+    let mounted = true;
+    getList()
+      .then(items => {
+        if (mounted) {
+          setList(items)
+        }
+      })
+    return () => mounted = false;
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItem(itemInput)
+    .then(() => {
+      setItemInput('');
+      setAlert(true);
+    })
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista zakupów</h1>
+      <ul>
+        {list.map(item => <li key={item.item}>{item.item}</li>)}
+      </ul>
+      {alert && <h2> Produkt dodany do listy </h2>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Nowy przedmiot</p>
+          <input type="text" onChange={event => setItemInput(event.target.value)} value={itemInput}/>
+        </label>
+        <button type="submit">Dodaj</button>
+      </form>
+
     </div>
-  );
+  )
 }
 
 export default App;
